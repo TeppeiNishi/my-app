202a9fdb7a2266ed71d435e317da778af64b6c7a
西 哲平 <teppei.nishi1@gmail.com>
2024-11-04 05:14:28 +0900

    no message

--- features/todo/views/TodoView.tsx
+++ features/todo/views/TodoView.tsx
@@ -1,48 +1,38 @@
 'use client'
 
-import { useMemo, useState } from 'react'
-
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { setTodoList } from '@/lib/features/todo/todoSlice'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMemo } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { TodoCard } from '../components/TodoCard'
 
+const formSchema = z.object({
+  task: z.string().min(1, {
+    message: 'Task is required',
+  }),
+})
+
 export function TodoView() {
-  const [newTodo, setNewTodo] = useState('')
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      task: '',
+    },
+  })
+
   const todoList = useAppSelector((state) => state.todo.todoList)
   const dispatch = useAppDispatch()
-
-  const handleAddTodo = () => {
-    if (newTodo.trim() === '') return
-    const updatedTodoList = [
-      ...todoList,
-      {
-        id: Date.now(),
-        text: newTodo,
-        completed: false,
-      },
-    ]
-    dispatch(setTodoList(updatedTodoList))
-    setNewTodo('')
-  }
-
-  const handleToggleComplete = (id: number) => {
-    const updatedTodoList = todoList.map((todo) =>
-      todo.id === id
-        ? {
-            ...todo,
-            completed: !todo.completed,
-          }
-        : todo
-    )
-    dispatch(setTodoList(updatedTodoList))
-  }
-
-  const handleDeleteTodo = (id: number) => {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id)
-    dispatch(setTodoList(updatedTodoList))
-  }
 
   const uncompletedTodoList = useMemo(
     () => todoList.filter((todo) => !todo.completed),
@@ -54,18 +44,59 @@ export function TodoView() {
     [todoList]
   )
 
+  function handleAddTodo(values: z.infer<typeof formSchema>) {
+    const updatedTodoList = [
+      ...todoList,
+      {
+        id: Date.now(),
+        text: values.task,
+        completed: false,
+      },
+    ]
+    dispatch(setTodoList(updatedTodoList))
+    form.reset()
+  }
+
+  function handleToggleComplete(id: number) {
+    const updatedTodoList = todoList.map((todo) =>
+      todo.id === id
+        ? {
+            ...todo,
+            completed: !todo.completed,
+          }
+        : todo
+    )
+    dispatch(setTodoList(updatedTodoList))
+  }
+
+  function handleDeleteTodo(id: number) {
+    const updatedTodoList = todoList.filter((todo) => todo.id !== id)
+    dispatch(setTodoList(updatedTodoList))
+  }
+
   return (
     <div className="flex flex-col gap-4 p-6 max-w-xl mx-auto w-full h-full">
       <h1 className="text-3xl">ToDo List</h1>
 
-      <div className="flex w-full max-w-sm items-center space-x-2">
-        <Input
-          value={newTodo}
-          placeholder="Add a new task"
-          onChange={(e) => setNewTodo(e.target.value)}
-        />
-        <Button onClick={handleAddTodo}>Add</Button>
-      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleAddTodo)}>
+          <div className="flex w-full max-w-sm space-x-2">
+            <FormField
+              control={form.control}
+              name="task"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Add a new task" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Add</Button>
+          </div>
+        </form>
+      </Form>
 
       <div className="flex flex-col gap-4 h-full">
         <TodoCard
